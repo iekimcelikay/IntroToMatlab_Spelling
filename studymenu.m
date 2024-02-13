5b9a2fc42312f92c6d71e2af49306011af37df11
Ekim <i.ekimcelikay@gmail.com>
2024-02-13 14:42:16 +0100

    kk replaced with idx, x_screen y_screen rep with x2 y2

--- studymenu.m
+++ studymenu.m
@@ -1,4 +1,4 @@
-function [] = studymenu(win,words,images_path,words_sound_path,letters_sound_path,deviceid,kk)
+function [] = studymenu(win,words,images_path,words_sound_path,letters_sound_path,deviceid,idx)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 % win = which PTB window this will work on
@@ -22,9 +22,9 @@ Screen('TextSize', win, 18);
 Screen('TextFont', win, 'Kristen ITC');
 Screen('flip', win);
 
-x_screen=1280;
-y_screen=800;
-size_of_main_window=[0 0 x_screen y_screen];
+x2=1280;
+y2=800;
+size_of_main_window=[0 0 x2 y2];
 bg_colour=[195 68 122];
 
 % set the rectangles on the screen
@@ -44,7 +44,7 @@ keepRunning=true;
 noClickYet = true;
 countClick = 0;
 
-if kk>= 1 && kk<=38
+if idx>= 1 && idx<=38
     while keepRunning
         [keyIsDown, ~, keyCode] = KbCheck;
         if keyIsDown
@@ -58,18 +58,18 @@ if kk>= 1 && kk<=38
                 return
             elseif keyCode(KbName('LeftArrow'))
                 Screen(win,'flip')
-                kk = kk -1;
+                idx = idx -1;
                 continue
             elseif keyCode(KbName('RightArrow'))
                 Screen(win,'flip')
-                kk = kk+1;
+                idx = idx+1;
                 continue
             end
         end
         [mouseX, mouseY, buttons] = GetMouse;  %%% while keepRunning = true this will run. 
 
     % Show the main image on the screen.
-    filename=fullfile([images_path, words{kk}, '.jpg']);
+    filename=fullfile([images_path, words{idx}, '.jpg']);
     myImage = imread(filename);
     tex=Screen('MakeTexture', win, myImage);
     Screen('DrawTexture', win, tex, [], image_rect);
@@ -89,7 +89,7 @@ if kk>= 1 && kk<=38
         if buttons(1) ==1
             % Button 1: See the word
             if mouseX > rect1(1) & mouseX<rect1(3) & mouseY>rect1(2) & mouseY<rect1(4)
-                newStr = upper(words{kk});
+                newStr = upper(words{idx});
                 DrawFormattedText(win, newStr, 'center' , 'center',[0 0 0], [], [], [], [], [], rect1+[0 50 0 50]);
                 Screen(win, 'Flip', [], 1);
                 countClick = countClick + 1;
@@ -98,7 +98,7 @@ if kk>= 1 && kk<=38
             % Button 2: Listen the word
             elseif mouseX > rect2(1) & mouseX<rect2(3) & mouseY>rect2(2) & mouseY<rect2(4)
                 % Loading the audio for word
-                audio_file = fullfile([words_sound_path, words{kk}, '.wav']);
+                audio_file = fullfile([words_sound_path, words{idx}, '.wav']);
                 [data, samplingRate]=audioread(audio_file);
                 pahandle = PsychPortAudio('Open', deviceid, [], [], samplingRate, 1);
                 PsychPortAudio('FillBuffer', pahandle, data');
@@ -108,7 +108,7 @@ if kk>= 1 && kk<=38
 
             % Button 3: See the spelling
             elseif  mouseX > rect3(1) & mouseX<rect3(3) & mouseY>rect3(2) & mouseY<rect3(4)
-                newStr = upper(words{kk});
+                newStr = upper(words{idx});
                 letters = {};
                 position = rect3 + [0 50 0 50];
                 for l=1:numel(newStr)
@@ -127,7 +127,7 @@ if kk>= 1 && kk<=38
             %Button 4: Listen the spelling
             elseif mouseX > rect4(1) & mouseX<rect4(3) & mouseY>rect4(2) & mouseY<rect4(4)
                 % Loading the audio for letters (spelling listening)
-                newStr = upper(words{kk});
+                newStr = upper(words{idx});
                 letters = {};
                 for l=1:numel(newStr)
                     letter = newStr(l);
